b999db590b3717a40700a877d0aa224904607362
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-04-06 11:20:14 -0700

    fixed syntax err.

--- public/admin.js
+++ public/admin.js
@@ -5,40 +5,37 @@ async function getBooks() {
     let response = await fetch('http://localhost:3001/listBooks');
 
     let books = await response.json();
-    books.forEach(renderBook);
+    books.forEach(renderBookList);
     console.log(books);
 }
-function renderBook(book) {
+function renderBookList(book) {
     let root = document.getElementById('root')
     let bookitem = document.createElement('li')
     bookitem.innerHTML= book.title;
     let quantitytext = document.createElement ('input')
     quantitytext.setAttribute('value', book.quantity)
     let updatebutton = document.createElement('button')
-
+    updatebutton.textContent= 'Update';
+    updatebutton.addEventListener('click', async ()=>{
+        let response = await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "id": book.id,   
+                "quantity": quantitytext.value
+                
+            })
+        });
+    
+        console.log(updatedBook);
+    })
+    bookitem.append(quantitytext)
+    bookitem.append(updatebutton)
+    root.append(bookitem)
 }
 async function updateBook() {
-    let response = await fetch('http://localhost:3001/updateBook', options, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': '/application/json'
-
-        },
-        body: JSON.stringify({
-            "id": 3,
-            "title": "The Annals of Arathrae",
-            "description": "This anthology tells the intertwined narratives of six fairy tales.",
-            "year": 2016,
-            "quantity": "8",
-            "imageURL": "/assets/arathrae.jpeg",
-
-            "error": "true",
-            "message": "'id' is required to call this method",
-        },
-        )
-    });
-
-    let updatedBook = await response.json();
-    console.log(updatedBook);
+    
 }
 getBooks()
